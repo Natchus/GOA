c7287ca6d1334538a0f49de094a1b2756da67e54
Natchus <nataliatchubabria@gmail.com>
2025-08-22 18:32:43 +0400

    second commit

--- day 20/homework/homework1.py
+++ day 20/homework/homework1.py
@@ -1,6 +1,5 @@
 name=input("enter your name:")
-for a in name:
-    if a.isupper():
-        print(name.lower())
-    else:
-        print(name.upper())
+if name.isupper():
+    print(name.lower())
+else:
+    print(name.upper())
---
+++ day 27/homework/homework.py
@@ -0,0 +1,6 @@
+my_word = "food"
+your_word = input("enter your word:")
+if your_word.lower() == my_word.lower():
+    print("we have the same word")
+else:
+    print("we have different words")
