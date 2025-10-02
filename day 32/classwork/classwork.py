9c268748c4bb2251ea782b1ff397a34dd1d1db4d
Natchus <nataliatchubabria@gmail.com>
2025-10-02 23:38:58 +0400

    second commit

---
+++ day 32/classwork/classwork.py
@@ -0,0 +1,3 @@
+def first_last(word):
+    return word[1:-1]
+print(first_last("play"))
---
+++ day 32/classwork/classwork1.py
@@ -0,0 +1,6 @@
+def list_length(list):
+    if len(list) > 4:
+        return "the list is too long"
+    else:
+        return list
+print(list_length([4, 6, 7, 7, 8]))
