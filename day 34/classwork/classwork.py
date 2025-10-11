a55e2671de3c620bda9ca5b3b7e4943a7dc0f575
Natchus <nataliatchubabria@gmail.com>
2025-10-11 13:20:13 +0400

    goa

---
+++ day 34/classwork/classwork.py
@@ -0,0 +1,6 @@
+fruit = {"apple", "grape", "watermelon"}
+fruit.add("pear")
+fruit.remove("apple")
+print(fruit)
+fruit.clear()
+print(fruit)
---
+++ day 34/classwork/classwork1.py
@@ -0,0 +1,8 @@
+students = ["Lisa", "Rosie", "Natalie", "Josh"]
+students.append("Pam")
+print(students)
+students.pop(2)
+print(students)
+len(students)
+print(len(students))
+print(students.count("Josh"))
